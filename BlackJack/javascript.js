c651bcff2d3e4d93d812e2d2d7d67cd584f4071e
Filip-Politowski <fil.politowski@gmail.com>
2023-02-22 19:20:28 +0100

    Added: - playAgain() function - getRandomCard() function - limiting new cards - rename function startGame() to renderGame() and refactor function startGame() - new boolean values added

--- BlackJack/javascript.js
+++ BlackJack/javascript.js
@@ -1,17 +1,40 @@
-let firstCard = Math.floor(Math.random() * 12) + 2
-let secondCard = Math.floor(Math.random() * 12) + 2
-sum = firstCard + secondCard
+let sum = 0
+let cards = []
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
-let arrayCards = {firstCard,secondCard}
+let isStarted = true
+let playerName = "Filip"
+let playerChips = 0
+
+let playerEl = document.getElementById("player-el")
+playerEl.textContent = playerName + ": " + playerChips
 
 function startGame() {
+    if (isStarted === true) {
+        isAlive = true
+        isStarted = false
+        let firstNumber = getRandomCard()
+        let secondNumber = getRandomCard()
+        cards = [firstNumber, secondNumber]
+        sum = firstNumber + secondNumber
+        renderGame()
+    }
+}
 
-    cardsEl.textContent = "Cards: " + firstCard + ", " + secondCard
+function renderGame() {
+
+    cardsEl.textContent = "Cards: "
+    for (let i = 0; i < cards.length; i++) {
+        if (i === cards.length - 1) {
+            cardsEl.textContent += cards[i]
+        } else {
+            cardsEl.textContent += cards[i] + ", "
+        }
+    }
     sumEl.textContent = "SUM: " + sum
 
     if (sum < 21) {
@@ -26,10 +49,30 @@ function startGame() {
     messageEl.textContent = message
 }
 
-function newCard(){
-let nextCard = Math.floor(Math.random() *12) + 2
-    sum += nextCard
-    startGame()
+function newCard() {
+    if (isAlive === true && hasBlackJack !== true) {
+        let nextCard = getRandomCard()
+        sum += nextCard
+        cards.push(nextCard)
+        console.log(cards)
+        renderGame()
+    }
+}
 
+function getRandomCard() {
+    let randomNumber = Math.floor(Math.random() * 13) + 1
+    if (randomNumber > 10) {
+        return 10
+    } else if (randomNumber === 1) {
+        return 11
+    } else {
+        return randomNumber
+    }
+}
+
+function playAgain() {
+    isStarted = true
+    hasBlackJack = false
+    startGame()
 
 }
