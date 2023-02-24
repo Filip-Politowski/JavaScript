25293b51f4655c4034dad5df744d0dd8568f4f77
Filip-Politowski <fil.politowski@gmail.com>
2023-02-24 20:24:13 +0100

    Added: - Saving to local storage - Delete button - Deleting from local storage - Pressing enter saves the inputs - Clearing array when delete button where clicked

--- chromeExtension/js.js
+++ chromeExtension/js.js
@@ -2,28 +2,52 @@ let myLeads = []
 const inputEl = document.getElementById('input-el')
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+const deleteButton = document.getElementById("delete-leads-btn")
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
 
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    renderLeads()
+}
 
 inputBtn.addEventListener("click", function () {
     myLeads.push(inputEl.value)
     inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
     renderLeads()
 
 })
 
 function renderLeads() {
-    const li = document.createElement("li")
-    li.setAttribute("id","leads-list")
+    let listItems = ""
     for (let i = 0; i < myLeads.length; i++) {
-        li.innerHTML = `<a id="leads-links" target ='_blank' href ='${myLeads[i]}'>  ${myLeads[i]} </a>`
+        listItems += `
+            <li>
+                <a id = "leads-links"target='_blank' href='${myLeads[i]}'>
+                    ${myLeads[i]}
+                </a>
+            </li>
+        `
     }
-    ulEl.append(li)
-
+    ulEl.innerHTML = listItems
 }
 
+deleteButton.addEventListener("click", function deleteAll() {
+    localStorage.clear()
+    myLeads = []
+    ulEl.textContent = ""
+})
 
 
-
-
+// Execute a function when the user presses a key on the keyboard
+inputEl.addEventListener("keypress", function (event) {
+    // If the user presses the "Enter" key on the keyboard
+    if (event.key === "Enter") {
+        // Cancel the default action, if needed
+        event.preventDefault();
+        // Trigger the button element with a click
+        document.getElementById("input-btn").click();
+    }
+});
 
